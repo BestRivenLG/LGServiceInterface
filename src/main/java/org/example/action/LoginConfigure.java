fc1dc3855e766b112853970de1d501f76ae98f23
BestRivenLG <373268232@qq.com>
2023-09-04 00:54:23 +0800

    拦截器错误处理

--- src/main/java/org/example/action/LoginConfigure.java
+++ src/main/java/org/example/action/LoginConfigure.java
@@ -1,9 +1,13 @@
 package org.example.action;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+@Configuration
 public class LoginConfigure implements WebMvcConfigurer {
 
     @Override
--- src/main/java/org/example/action/UserLoginInterceptor.java
+++ src/main/java/org/example/action/UserLoginInterceptor.java
@@ -1,8 +1,13 @@
 package org.example.action;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.example.entity.Account;
+import org.example.mapper.AccountMapper;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,7 +16,13 @@ public class UserLoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("执行了拦截器的preHandle方法");
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+        boolean isVaild = getHeaderToken(request);
+        if (isVaild) {
+            return  true;
+        } else {
+            return  false;
+        }
+//        return getHeaderToken(request);
     }
 
     @Override
@@ -22,5 +33,28 @@ public class UserLoginInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         System.out.println("执行了拦截器的afterCompletion方法");
+    }
+
+    @Resource
+    AccountMapper accountMapper;
+
+    private Account tokenIsVaild(String token) {
+        QueryWrapper<Account> query = new QueryWrapper<Account>();
+        query.eq("token", token);
+        query.select("id", "nickname"); // 指定要返回的字段
+        query.last("limit 1");
+        return accountMapper.selectOne(query);
+    }
+
+    private Boolean getHeaderToken(HttpServletRequest httpServletRequest){
+        {
+            String token = httpServletRequest.getHeader("token");
+            Account account = tokenIsVaild(token);
+            boolean isVail = account != null;
+            String content = isVail ? "已登录" : "未登录";
+            System.out.println(content);
+            return  isVail;
+        }
+
     }
 }
