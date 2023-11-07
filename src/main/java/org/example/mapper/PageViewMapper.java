ae5a57bf49a15473c6900f6235fcfe5b34279258
Yaro <yaro@YarodeMacBook-Pro.local>
2023-11-07 20:28:35 +0800

    查询浏览记录SQL语句

--- src/main/java/org/example/mapper/PageViewMapper.java
+++ src/main/java/org/example/mapper/PageViewMapper.java
@@ -11,7 +11,9 @@ import org.example.entity.Photo;
 public interface PageViewMapper extends BaseMapper<PageView> {
 
 
-    @Select("SELECT p.id, p.category_id, COALESCE(p.`collect` , pc.user_id  = pv.user_id) AS collect, p.icon, p.ratio, p.title, p.title, p.resource_type, p.video_url, p.page_view, p.page_text, p.images, p.en_title  FROM photos p JOIN page_view pv ON p.id = pv.photo_id JOIN photos_collect pc ON p.id = pc.photo_id AND pc.user_id = pv.user_id WHERE  pv.user_id  = #{userId} ORDER BY pv.update_time DESC")
+
+    @Select("SELECT p.id, p.category_id, (pc.user_id = #{userId} AND p.id = pc.photo_id) AS collect, p.icon, p.ratio, p.title, p.resource_type, p.video_url, p.page_view, p.page_text, p.images, p.en_title FROM photos p LEFT JOIN photos_collect pc ON p.id = pc.photo_id AND pc.user_id = #{userId} JOIN page_view pv ON p.id = pv.photo_id AND pv.user_id = #{userId} ORDER BY pv.update_time DESC")
+//    @Select("SELECT p.id, p.category_id, COALESCE(p.`collect` , pc.user_id  = pv.user_id) AS collect, p.icon, p.ratio, p.title, p.title, p.resource_type, p.video_url, p.page_view, p.page_text, p.images, p.en_title  FROM photos p JOIN page_view pv ON p.id = pv.photo_id JOIN photos_collect pc ON p.id = pc.photo_id AND pc.user_id = pv.user_id WHERE  pv.user_id  = #{userId} ORDER BY pv.update_time DESC")
 //    @Select("SELECT p.id, p.category_id, p.icon, COALESCE(p.`collect` , TRUE) AS collect, p.ratio, p.title, p.title, p.resource_type, p.video_url, p.page_view, p.page_text, p.images, p.en_title  FROM photos p JOIN photos_collect pc ON p.id = pc.photo_id AND pc.user_id  = #{userId} WHERE p.resource_type = #{resourceType} ORDER BY pc.create_time DESC")
 //    @Select("SELECT *, (CASE WHEN p.collect IS NULL THEN true ELSE false END) AS collect FROM photos p JOIN photos_collect pc ON p.id = pc.photo_id AND pc.user_id  = #{userId} WHERE p.resource_type = #{resourceType}")
 //    @Select("SELECT *, (CASE WHEN p.is_collect IS NULL THEN true ELSE false END) AS is_collect FROM photos p JOIN photos_collect pc ON p.id = pc.photo_id AND pc.user_id  = #{userId} WHERE p.resource_type = #{resourceType}")
