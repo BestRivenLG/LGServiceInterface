4e43ee3cf9246d3936baa42b1b3ddffafaf3f7e8
Yaro <yaro@YarodeMacBook-Pro.local>
2023-11-13 20:37:34 +0800

    图片列表

--- src/main/java/org/example/action/zxz/ZXPhototActions.java
+++ src/main/java/org/example/action/zxz/ZXPhototActions.java
@@ -1,17 +1,16 @@
 package org.example.action.zxz;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.example.common.CommonTool;
 import org.example.common.RespErrorCode;
-import org.example.entity.Account;
-import org.example.entity.BannerEntity;
-import org.example.entity.PhotoCategory;
-import org.example.entity.RespResult;
+import org.example.entity.*;
+import org.example.mapper.AccountMapper;
 import org.example.mapper.BannerMapper;
 import org.example.mapper.PhotoCategoryMapper;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.mapper.PhotoMapper;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -20,17 +19,20 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/zxz/category")
+@RequestMapping("/api")
 public class ZXPhototActions {
 
     @Resource
     BannerMapper bannerMapper;
-
     @Resource
     PhotoCategoryMapper categoryMapper;
+    @Resource
+    AccountMapper accountMapper;
+    @Resource
+    PhotoMapper photoMapper;
 
     @CrossOrigin(origins = "*") // 设置允许来自任何源的跨域请求
-    @GetMapping("/index")
+    @GetMapping("/zxz/category/index")
     public RespResult<Map<String, Object>> homeIndex(HttpServletRequest request) {
         QueryWrapper<BannerEntity> query = new QueryWrapper<BannerEntity>();
         query.eq("type", 1);
@@ -54,4 +56,44 @@ public class ZXPhototActions {
         result.setMsg(RespErrorCode.OK.getMessage());
         return  result;
     }
+
+    @CrossOrigin(origins = "*") // 设置允许来自任何源的跨域请求
+    @GetMapping("/resource/home")
+    public RespResult<IPage<Photo>> getPhotoListV3(HttpServletRequest request,
+                                                   @RequestParam(value = "categoryId", required = false) Long categoryId,
+                                                   @RequestParam(value = "page", defaultValue = "1") Long page,
+                                                   @RequestParam(value = "size", defaultValue = "5") Long size) {
+        RespResult<IPage<Photo>> result = new RespResult<>();
+        Page<Photo> ipage = new Page<>(page, size);
+        Account account = CommonTool.tokenIsVaild(accountMapper, request);
+        if (account == null) {
+            QueryWrapper<Photo> query = new QueryWrapper<Photo>();
+            if (categoryId != null) {
+                /// 字段需要跟数据库字段对应，不能使用驼峰 categoryId, 查询异常
+                query.eq("category_id", categoryId);
+            }
+            Page<Photo> listss = photoMapper.selectPage(ipage, query);
+            result.setData(listss);
+            result.setCode(RespErrorCode.OK.getCode());
+            result.setStatus(RespErrorCode.OK.getStatus());
+            result.setMsg(RespErrorCode.OK.getMessage());
+            return result;
+        }
+        Page<Photo> pagePhoto;
+        if (categoryId == null) {
+            pagePhoto = photoMapper.selectPageMyAllPhotoList(ipage, account.getId());
+        } else  {
+            if (categoryId == 4) { //最近浏览
+                pagePhoto = photoMapper.selectPageMyPhotoList(ipage, categoryId, account.getId());
+            } else {
+                pagePhoto = photoMapper.selectPageMyPhotoList(ipage, categoryId, account.getId());
+            }
+        }
+        result.setData(pagePhoto);
+        result.setCode(RespErrorCode.OK.getCode());
+        result.setStatus(RespErrorCode.OK.getStatus());
+        result.setMsg(RespErrorCode.OK.getMessage());
+        return result;
+    }
+
 }
