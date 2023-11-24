7dbdb9e2b76c3fe3c1ec9d844617096fb5e8d574
BestRivenLG <373268232@qq.com>
2023-11-25 02:19:45 +0800

    新增打包脚步

---
+++ build.sh
@@ -0,0 +1,37 @@
+echo 'package finished'
+mvn clean package
+echo '开始后台运行程序'
+current_path=$(readlink -f .)
+
+last_path=$(readlink -f ..)
+
+log_dir="$last_path/LGService"
+log_path="$last_path/LGService/LGService.log"
+if [[ -d $log_dir ]]; then
+    echo "找到 JAR 文件: $target"
+else
+    mkdir $log_path
+    echo "没有找到 JAR 文件"
+fi
+
+#target="$current_path/target/JavaTestDemo-1.0-SNAPSHOT.jar"
+target=$(find "$current_path/target" -maxdepth 1 -name "*.jar" | head -n 1)
+
+if [[ -f "$target" ]]; then
+    echo "找到 JAR 文件: $target"
+
+    # 在这里执行对 JAR 文件的操作
+    echo "结束之前进程"
+    pkill -f "java -jar $target"
+    echo "结束之前进程 完成"
+    
+    echo "开始后台运行jar程序"
+    nohup java -jar $target > $log_path &
+    echo "后台运行完成"
+
+    # 查看进程
+    ps -ef|grep java
+else
+    echo "没有找到 JAR 文件"
+fi
+
