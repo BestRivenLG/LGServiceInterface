1b49b454d5f0174d638bc317bcbc54769e770337
BestRivenLG <373268232@qq.com>
2023-11-25 03:27:48 +0800

    发布脚步

--- pushCommand.sh
+++ pushCommand.sh
@@ -1,5 +1,5 @@
 
 ssh -t root@www.lopcool.com "sh /usr/product/java/LGServices/build.sh"
-#ssh -t root@api.lopcool.com "sh /usr/product/java/LGServices/build.sh"
-#ssh -t root@nlb.lopcool.com "sh /usr/product/java/LGServices/build.sh"
-#ssh -t root@8.134.191.74 "sh /usr/product/java/LGServices/build.sh"
+ssh -t root@api.lopcool.com "sh /usr/product/java/LGServices/build.sh"
+ssh -t root@nlb.lopcool.com "sh /usr/product/java/LGServices/build.sh"
+ssh -t root@8.134.191.74 "sh /usr/product/java/LGServices/build.sh"
